23aff443181f796a9d214365386c007c0e7e2025
zummon <zummon.space@gmail.com>
2023-04-27 20:05:36 +0700

    trying to fix file not found

--- src/routes/about/+page.server.js
+++ src/routes/about/+page.server.js
@@ -1,8 +1,15 @@
 import matter from 'gray-matter';
+import fs from 'fs';
 
 export const load = async ({}) => {
 	// const { data, content } = matter.read('../../lib/content/about.md');
-	console.log(await import('../../lib/content/about.md'));
+	// console.log(await import('../../lib/content/about.md'));
+	fs.readFile('../../lib/content/about.md', 'utf8', (err, data) => {
+		if (err) throw err;
+		console.log(data);
+	});
+	let data = {},
+		content;
 
 	return { ...data, content };
 };
